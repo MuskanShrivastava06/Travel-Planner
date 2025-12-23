51baf0d26dfaeb51ff3342e7e1ea3fa6d6349f07
Muskan06-star <shrivastavamuskan45@gmail.com>
2025-12-23 10:10:31 +0530

    day4 done

--- src/JS/destination.js
+++ src/JS/destination.js
@@ -1,13 +1,69 @@
-const params= new URLSearchParams(window.location.search);
+const params = new URLSearchParams(window.location.search);
+const city = params.get("city");
+const cityName = document.getElementById("cityName");
+const loading = document.querySelector("#loading");
+const cityInfoBox=document.querySelector("#cityInfo");
+const errorBox=document.querySelector("#error");
 
-const city=params.get("city");
+const lat=document.querySelector("#lat");
+const lon=document.querySelector("#lon");
+const country=document.querySelector("#country");
+const state=document.querySelector("#state");
+const population=document.querySelector("#population");
 
 
-const cityName=document.getElementById("cityName")
+let GEODB_API_KEY = "5ced1531e3msh79e40622b912917p17bb63jsn246609c6dee2";
+let GEODB_HOST = "wft-geo-db.p.rapidapi.com";
+if (city) {
+    cityName.textContent = city;
 
-if(city){
-    cityName.textContent=city;
+    async function fetchCityDetails(cityName) {
+        try {
+            let result = await fetch(
+                `https://${GEODB_HOST}/v1/geo/cities?namePrefix=${city}&limit=1`,
+                {
+                    method: "GET",
+                    headers: {
+                        "X-RapidAPI-Key": GEODB_API_KEY,
+                        "X-RapidAPI-HOST": GEODB_HOST,
+                    },
+                }
+            );
+            console.log(result);
+
+            if (!result.ok) {
+                console.log("API Response Not Fetched...");
+
+            }
+            let data = await result.json()
+            console.log(data);
+
+            if (!data.data || data.data.length === 0) {
+                console.log("City Not Found...");
+
+            }
+            let cityData = data.data[0]
+            console.log(cityData);
+
+            lat.textContent = cityData.latitude;
+            lon.textContent = cityData.longitude;
+            country.textContent = cityData.country;
+            state.textContent = cityData.region || "N/A";
+            population.textContent = cityData.population? cityData.population.toLocaleString() : "N/A";
+
+            loading.classList.add("hidden");
+            cityInfoBox.classList.remove("hidden");
+
+
+        } catch (error) {
+            console.log(error);
+            loading.classList.add("hidden");
+            errorBox.classList.remove("hidden");
+        }
+    }
+    fetchCityDetails(city);
+} else {
+    cityName.textContent = "Unknown City";
+    loading.classList.add("hidden");
 }
-else{
-    cityName.textContent="Unknown City";
-}
+
